a3cb1dc499e9a4b63f02adbbead138da2a0f929f
Garry3215 <45031592+Garry3215@users.noreply.github.com>
2019-03-13 13:37:06 +0300

    добавил условие

--- notify.php
+++ notify.php
@@ -24,12 +24,14 @@ $sql = "SELECT user_id, GROUP_CONCAT(name SEPARATOR ', ') as name FROM task WHER
 $res = mysqli_query($con, $sql);
 $result = mysqli_fetch_all($res, MYSQLI_ASSOC);
 $number = mysqli_num_rows($res);
-foreach ($result as $key => $value) {
-    $current = $value['user_id'];
-    $sql = "SELECT * FROM users WHERE id = '$current'";
-    $res = mysqli_query($con, $sql);
-    $result_current = mysqli_fetch_assoc($res);
-    $message->setTo([$result_current['email'] => $result_current['name']]);
-    $message->setBody("Сегодня истекает срок исполнения следующих задач: " . $value['name']);
-    $mailer->send($message);
+if ($number > 0) {
+    foreach ($result as $key => $value) {
+        $current = $value['user_id'];
+        $sql = "SELECT * FROM users WHERE id = '$current'";
+        $res = mysqli_query($con, $sql);
+        $result_current = mysqli_fetch_assoc($res);
+        $message->setTo([$result_current['email'] => $result_current['name']]);
+        $message->setBody("Сегодня истекает срок исполнения следующих задач: " . $value['name']);
+        $mailer->send($message);
+    }
 }
