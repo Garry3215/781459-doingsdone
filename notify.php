40423f7dd476134c542232a3f88f86343bff3104
=Vladislav Malinichev <=malinichev.ru@gmail.com>
2019-03-14 22:30:18 +0200

    Update notify.php

--- notify.php
+++ notify.php
@@ -1,25 +1,40 @@
 <?php
+
 require_once 'init.php';
+
 $transport = new Swift_SmtpTransport('phpdemo.ru', 25);
 $transport->setUsername('keks@phpdemo.ru');
 $transport->setPassword('htmlacademy');
+
 $message = new Swift_Message("Уведомление от сервиса «Дела в порядке»");
 $message->setFrom("keks@phpdemo.ru", "htmlacademy");
 $mailer = new Swift_Mailer($transport);
-$cur_date = strtotime('today');
-$cur_date = date("Y-m-d", $cur_date);
-$sql = "SELECT user_id, GROUP_CONCAT(name SEPARATOR ', ') as name FROM task WHERE status = 0 AND date_must_done = '$cur_date' GROUP BY user_id";
+
+$start_date = date('Y-m-d H:i:s', time());
+$end_date = date('Y-m-d H:i:s', strtotime('+1 day'));
+$sql = "SELECT u.name AS user_name, u.email AS user_email, u.id AS user_id, t.name, t.date_must_done FROM task t LEFT JOIN users u ON u.id = t.user_id WHERE t.status = 0 AND t.date_must_done BETWEEN '$start_date' AND '$end_date'";
+
 $res = mysqli_query($con, $sql);
-$result = mysqli_fetch_all($res, MYSQLI_ASSOC);
-$number = mysqli_num_rows($res);
-if ($number > 0) {
-    foreach ($result as $key => $value) {
-        $current = $value['user_id'];
-        $sql = "SELECT * FROM users WHERE id = '$current'";
-        $res = mysqli_query($con, $sql);
-        $result_current = mysqli_fetch_assoc($res);
-        $message->setTo([$result_current['email'] => $result_current['name']]);
-        $message->setBody("Уважаемый, " . $result_current['name'] . ". У вас запланирована задача (задачи): " . $value['name']);
+
+$array = [];
+while ($row = mysqli_fetch_assoc($res)) {
+	if (!isset($array[$row['user_id']])) {
+		$array[$row['user_id']] = [
+			'name' => $row['user_name'],
+			'email' => $row['user_email'],
+		];
+	}
+	$array[$row['user_id']]['tasks'][] = $row;
+}
+
+if (!empty($array)) {
+    foreach ($array as $key => $row) {
+        $list = [];
+		foreach ($row['tasks'] as $task) {
+			$list[] = $task['name'] . ' на ' . date('H:i', strtotime($task['date_must_done']));
+		}
+        $message->setTo([$row['email'] => $row['name']]);
+        $message->setBody("Уважаемый, " . $row['name'] . ". У вас запланирована задача (задачи): " . implode(', ', $list));
         $mailer->send($message);
     }
 }
