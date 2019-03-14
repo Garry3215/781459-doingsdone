af0f8d6deb1e73066fb7b6abfdd847a1537b734a
Garry3215 <45031592+Garry3215@users.noreply.github.com>
2019-03-14 22:19:42 +0300

    корректное сообщение

--- notify.php
+++ notify.php
@@ -12,9 +12,13 @@ $mailer = new Swift_Mailer($transport);
 
 $cur_date = strtotime('today');
 $cur_date = date("Y-m-d", $cur_date);
-$sql = "SELECT user_id, GROUP_CONCAT(name SEPARATOR ', ') as name FROM task WHERE status = 0 AND date_must_done = '$cur_date' GROUP BY user_id";
+$sql = "SELECT user_id, name, date_must_done FROM task WHERE status = 0 AND date_must_done = '$cur_date'";
 $res = mysqli_query($con, $sql);
 $result = mysqli_fetch_all($res, MYSQLI_ASSOC);
+$list = [];
+foreach ($result as $row) {
+  $list[] = $row['name'] . ' на ' . date('H:i', strtotime($row['date_must_done']));
+}
 $number = mysqli_num_rows($res);
 if ($number > 0) {
     foreach ($result as $key => $value) {
@@ -23,7 +27,7 @@ if ($number > 0) {
         $res = mysqli_query($con, $sql);
         $result_current = mysqli_fetch_assoc($res);
         $message->setTo([$result_current['email'] => $result_current['name']]);
-        $message->setBody("Уважаемый, " . $result_current['name'] . ". У вас запланирована задача (задачи): " . $value['name']);
+        $message->setBody("Уважаемый, " . $result_current['name'] . ". У вас запланирована задача (задачи): " . implode(', ', $list));
         $mailer->send($message);
     }
 }
